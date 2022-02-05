5e0fd576bbb25f8fea866c189374f295fd667adc
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-02-05 20:55:37 +0500

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -11,25 +11,73 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("ali raza"),
-        ),
-        body: SafeArea(
-          child: Container(
-            color: Colors.yellow,
-            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.start,
+        home: Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Icon(Icons.stars, size: 50),
-                Icon(Icons.stars, size: 50),
-                Icon(Icons.stars, size: 50),
+                Container(
+                  color: Colors.amberAccent,
+                  width: 201,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 201,
+                  height: 200,
+                ),
               ],
             ),
-          ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      image: DecorationImage(
+                        image: AssetImage('image/bulb.jpg'),
+                        fit: BoxFit.cover,
+                      ),
+                      border: Border.all(
+                        color: Colors.black,
+                        width: 8,
+                      ),
+                      borderRadius: BorderRadius.circular(150),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.white.withOpacity(0.5),
+                          spreadRadius: 25,
+                          blurRadius: 15,
+                        ),
+                      ]),
+                  width: 196,
+                  height: 196,
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Container(
+                  color: Colors.purple,
+                  width: 201,
+                  height: 200,
+                ),
+                Container(
+                  color: Colors.lightGreenAccent,
+                  width: 201,
+                  height: 200,
+                ),
+              ],
+            ),
+          ],
         ),
       ),
-    );
+    ));
   }
 }
