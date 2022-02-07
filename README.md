8cab68b6b3452c10fbf42f5dbc308b51d77ac267
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-02-08 01:22:56 +0500

    my testemony

--- README.md
+++ README.md
@@ -17,3 +17,4 @@ samples, guidance on mobile development, and a full API reference.
 
 
 ![!MOBILE](image/mobile.png)
+![!my image](image/imagemy.png)
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+// import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+// import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,148 +14,281 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.black,
-          body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 185,
-                      width: 185,
-                      color: Colors.red,
-                    ),
-                    Container(
-                      height: 185,
-                      width: 185,
-                      color: Colors.green,
-                    )
-                  ],
-                ),
-                Container(
-                  width: 170,
-                  height: 170,
-                  decoration: BoxDecoration(
-                   boxShadow:[ BoxShadow(color: Colors.yellow,spreadRadius: 25,
-                   blurRadius: 15)],
+        backgroundColor: Colors.yellow.shade100,
+        body: SafeArea(
+          child: Column(
+            // crossAxisAlignment: CrossAxisAlignment.center,
+             mainAxisAlignment: MainAxisAlignment.center,
+
+            children: [
+              SizedBox(
+                child: (Card(
                     color: Colors.white,
-                    borderRadius: BorderRadius.circular(150),
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 4
-                    ),
-                    image: DecorationImage(
-                      image: AssetImage('image/bulb.jpg'),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 185,
-                      width: 185,
-                      color: Colors.pink,
-                    ),
-                    Container(
-                      height: 185,
-                      width: 185,
-                      color: Colors.purple,
-                    )
-                  ],
-                )
-              ],
-            ),
-          )),
+                    shadowColor: Colors.white,
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 90,
+                          width: 90,
+                          margin: new EdgeInsets.symmetric(vertical: 15.0),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(150),
+                              border: Border.all(
+                                  color: Colors.cyan.shade800,
+                                  width: 3 // red as border color
+                                  ),
+                              image: DecorationImage(
+                                image: AssetImage('image/mypic.jpg'),
+                                fit: BoxFit.cover,
+                              )),
+                        ),
+                        Text(
+                          "Ali-Raza",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w800,
+                            color: Colors.black38,
+                            fontFamily: 'Quintessential',
+                          ),
+                        ),
+                        Text(
+                          "Software Engineer",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.grey,
+                            fontFamily: 'Quintessential',
+                          ),
+                        ),
+                        Container(
+                          height: 35,
+                          width: 145,
+                          decoration: BoxDecoration(
+                            border: Border(
+                              bottom: BorderSide(
+                                width: 3.0,
+                                color: Colors.yellow.shade200,
+                              ),
+                            ),
+                            // color: Colors.white,
+                          ),
+                        ),
+                        Container(
+                          child: Icon(
+                            Icons.format_quote_outlined,
+                            color: Colors.yellow,
+                            size: 42,
+                          ),
+                        ),
+                        Container(
+                            child: Padding(
+                          padding: const EdgeInsets.all(20.3),
+                          child: (Text(
+                            "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available",
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 5,
+                            textAlign: TextAlign.center,
+                          )),
+                        )),
+                        Container(
+                          child: Icon(
+                            Icons.format_quote_outlined,
+                            color: Colors.yellow,
+                            size: 42,
+                          ),
+                        )
+                      ],
+                    ))),
+              ),
+
+              // Container(
+              //   height: 60,
+              //   width: 60,
+              //   decoration: BoxDecoration(
+              //       borderRadius: BorderRadius.circular(150),
+              //       image: DecorationImage(
+              //         image: AssetImage('image/mypic.jpg'),
+              //         fit: BoxFit.cover,
+              //       )),
+              // ),
+              // Text(
+              //   "Ali-Raza",
+              //   style: TextStyle(
+              //     fontSize: 18,
+              //     fontWeight: FontWeight.w800,
+              //     color: Colors.black38,
+              //     fontFamily: 'Quintessential',
+              //   ),
+              // ),
+              // Text(
+              //   "Software Engineer",
+              //   style: TextStyle(
+              //     fontSize: 18,
+              //     color: Colors.grey,
+              //     fontFamily: 'Quintessential',
+              //   ),
+              // ),
+              // Container(
+              //   height: 35,
+              //   width: 145,
+              //   decoration: BoxDecoration(
+              //     border: Border(
+              //       bottom: BorderSide(
+              //         width: 3.0,
+              //         color: Colors.yellow.shade200,
+              //       ),
+              //     ),
+              //     // color: Colors.white,
+              //   ),
+              // ),
+              // Container(
+              //   child: Icon(
+              //     Icons.format_quote_outlined,
+              //     color: Colors.yellow,
+              //     size: 42,
+              //   ),
+              // ),
+              // Container(
+              //     child: (Text(
+              //   "n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available",
+              //   textAlign: TextAlign.left,
+              //   overflow: TextOverflow.ellipsis,
+              //   maxLines: 3,
+              // ))),
+              // Container(
+              //   child: Icon(
+              //     Icons.format_quote_outlined,
+              //     color: Colors.yellow,
+              //     size: 42,
+              //   ),
+              // )
+            ],
+          ),
+        ),
+
+        // backgroundColor: Colors.black,
+        // body: SafeArea(
+        //   child: Column(
+        //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //     children: [
+        //       // Row(
+        //       //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //       //   children: [
+        //       //     Container(
+        //       //       child: Icon(Icons.pets_outlined,
+        //       //           color: Colors.yellow, size: 40),
+        //       //       height: 185,
+        //       //       width: 185,
+        //       //       color: Colors.red,
+        //       //     ),
+        //       //     Container(
+        //       //       child: Icon(Icons.restart_alt_outlined,
+        //       //           color: Colors.yellow, size: 40),
+        //       //       // Icon(
+        //       //       //   Icons.restart_alt_outlined,
+        //       //       // ),
+        //       //       height: 185,
+        //       //       width: 185,
+        //       //       color: Colors.green,
+        //       //     )
+        //       //   ],
+        //       // ),
+        //       // SizedBox(
+        //       //   height: 190,
+        //       //   width: 400,
+        //       //   child: Card(
+        //       //     color: Colors.white,
+        //       //     elevation: 20,
+        //       //     shadowColor: Colors.white,
+        //       //     child: Row(
+        //       //       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        //       //       children: [
+        //       //         Container(
+        //       //           width: 144,
+        //       //           height: 144,
+        //       //           decoration: BoxDecoration(
+        //       //             boxShadow: [
+        //       //               BoxShadow(
+        //       //                   color: Colors.yellow,
+        //       //                   spreadRadius: 25,
+        //       //                   blurRadius: 15)
+        //       //             ],
+        //       //             color: Colors.white,
+        //       //             borderRadius: BorderRadius.circular(150),
+        //       //             border: Border.all(color: Colors.black, width: 4),
+        //       //             image: DecorationImage(
+        //       //               image: AssetImage('image/bulb.jpg'),
+        //       //               fit: BoxFit.cover,
+        //       //             ),
+        //       //           ),
+        //       //         ),
+        //       //         Column(
+        //       //           mainAxisAlignment: MainAxisAlignment.center,
+        //       //
+        //       //           children: [
+        //       //             Text(
+        //       //               "Idea!!",
+        //       //               style: TextStyle(
+        //       //                 fontSize: 24,
+        //       //                 color: Colors.lightGreen,
+        //       //                 fontFamily: 'Quintessential',
+        //       //               ),
+        //       //             ),
+        //       //             LimitedBox(
+        //       //               maxHeight: 60,
+        //       //               maxWidth: 120,
+        //       //               child: ListTile(
+        //       //               horizontalTitleGap: 0,
+        //       //                 minLeadingWidth: 30,
+        //       //                 leading: Icon(
+        //       //                   Icons.facebook,
+        //       //                   color: Colors.red,
+        //       //
+        //       //                 ),
+        //       //                 title: Text(
+        //       //                   "@Idea",
+        //       //                   style: TextStyle(
+        //       //                     color: Colors.blue,
+        //       //                     fontSize: 15,
+        //       //                   ),
+        //       //                 ),
+        //       //               ),
+        //       //             )
+        //       //
+        //       //             // Icon(Icons.dark_mode_outlined,
+        //       //             //     color: Colors.yellow, size: 40),
+        //       //             // Text('facebook')
+        //       //           ],
+        //       //         ),
+        //       //       ],
+        //       //     ),
+        //       //   ),
+        //       // ),
+        //       // Row(
+        //       //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //       //   children: [
+        //       //     Container(
+        //       //       child: Icon(Icons.star_rate_outlined,
+        //       //           color: Colors.yellow, size: 40),
+        //       //       height: 185,
+        //       //       width: 185,
+        //       //       color: Colors.pink,
+        //       //     ),
+        //       //     Container(
+        //       //       child: Icon(Icons.insights_outlined,
+        //       //           color: Colors.yellow, size: 40),
+        //       //       // Icon(
+        //       //       //   Icons.insights_outlined,
+        //       //       // ),
+        //       //       height: 185,
+        //       //       width: 185,
+        //       //       color: Colors.purple,
+        //       //     )
+        //       //   ],
+        //       // )
+        //     ],
+        //   ),
+        // )
+      ),
     );
   }
 }
-// class MyApp extends StatelessWidget {
-//   const MyApp({Key? key}) : super(key: key);
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//         home: Scaffold(
-//       backgroundColor: Colors.black,
-//       body: SafeArea(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//               children: [
-//                 Container(
-//                   color: Colors.amberAccent,
-//                   width: 201,
-//                   height: 200,
-//                 ),
-//                 Container(
-//                   color: Colors.blue,
-//                   width: 201,
-//                   height: 200,
-//                 ),
-//               ],
-//             ),
-//             // Row(
-//             //   mainAxisAlignment: MainAxisAlignment.center,
-//             //   children: [
-//             // Row(
-//             //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//             //   children: [
-//             //     Container(
-//             //       decoration: BoxDecoration(
-//             //           color: Colors.white,
-//             //           image: DecorationImage(
-//             //             image: AssetImage('image/newbulb.jpg'),
-//             //             fit: BoxFit.cover,
-//             //           ),
-//             //           border: Border.all(
-//             //             color: Colors.black,
-//             //             width: 8,
-//             //           ),
-//             //           borderRadius: BorderRadius.circular(150),
-//             //           boxShadow: [
-//             //             BoxShadow(
-//             //               color: Colors.white.withOpacity(0.5),
-//             //               spreadRadius: 25,
-//             //               blurRadius: 15,
-//             //             ),
-//             //           ]),
-//             //       width: 196,
-//             //       height: 196,
-//             //     ),
-//             //     // Text(
-//             //     //   'Hello World',
-//             //     //   style: TextStyle(fontSize: 24, color: Colors.white),
-//             //     // ),
-//             //   ],
-//             // ),
-//
-//             // ],
-//             // ),
-//             // Row(
-//             //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//             //   mainAxisSize: MainAxisSize.max,
-//             //   children: [
-//             //     Container(
-//             //       color: Colors.purple,
-//             //       width: 201,
-//             //       height: 200,
-//             //     ),
-//             //     Container(
-//             //       color: Colors.lightGreenAccent,
-//             //       width: 201,
-//             //       height: 200,
-//             //     ),
-//             //   ],
-//             // ),
-//           ],
-//         ),
-//       ),
-//     ));
-//   }
-// }
