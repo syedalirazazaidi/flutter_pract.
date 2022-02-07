8f9641a128b034ba94f3082b16aa06bf0494ddfc
syedalirazazaidi <46336938+syedalirazazaidi@users.noreply.github.com>
2022-02-07 10:02:23 +0500

    rebuild app

--- lib/main.dart
+++ lib/main.dart
@@ -2,82 +2,156 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
+  // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      backgroundColor: Colors.black,
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
+      home: Scaffold(
+          backgroundColor: Colors.black,
+          body: SafeArea(
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Container(
-                  color: Colors.amberAccent,
-                  width: 201,
-                  height: 200,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 185,
+                      width: 185,
+                      color: Colors.red,
+                    ),
+                    Container(
+                      height: 185,
+                      width: 185,
+                      color: Colors.green,
+                    )
+                  ],
                 ),
                 Container(
-                  color: Colors.blue,
-                  width: 201,
-                  height: 200,
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
+                  width: 170,
+                  height: 170,
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      image: DecorationImage(
-                        image: AssetImage('image/bulb.jpg'),
-                        fit: BoxFit.cover,
-                      ),
-                      border: Border.all(
-                        color: Colors.black,
-                        width: 8,
-                      ),
-                      borderRadius: BorderRadius.circular(150),
-                      boxShadow: [
-                        BoxShadow(
-                          color: Colors.white.withOpacity(0.5),
-                          spreadRadius: 25,
-                          blurRadius: 15,
-                        ),
-                      ]),
-                  width: 196,
-                  height: 196,
+                   boxShadow:[ BoxShadow(color: Colors.yellow,spreadRadius: 25,
+                   blurRadius: 15)],
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(150),
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 4
+                    ),
+                    image: DecorationImage(
+                      image: AssetImage('image/bulb.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 185,
+                      width: 185,
+                      color: Colors.pink,
+                    ),
+                    Container(
+                      height: 185,
+                      width: 185,
+                      color: Colors.purple,
+                    )
+                  ],
+                )
               ],
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Container(
-                  color: Colors.purple,
-                  width: 201,
-                  height: 200,
-                ),
-                Container(
-                  color: Colors.lightGreenAccent,
-                  width: 201,
-                  height: 200,
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
-    ));
+          )),
+    );
   }
 }
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//   // This widget is the root of your application.
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//       backgroundColor: Colors.black,
+//       body: SafeArea(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 Container(
+//                   color: Colors.amberAccent,
+//                   width: 201,
+//                   height: 200,
+//                 ),
+//                 Container(
+//                   color: Colors.blue,
+//                   width: 201,
+//                   height: 200,
+//                 ),
+//               ],
+//             ),
+//             // Row(
+//             //   mainAxisAlignment: MainAxisAlignment.center,
+//             //   children: [
+//             // Row(
+//             //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+//             //   children: [
+//             //     Container(
+//             //       decoration: BoxDecoration(
+//             //           color: Colors.white,
+//             //           image: DecorationImage(
+//             //             image: AssetImage('image/newbulb.jpg'),
+//             //             fit: BoxFit.cover,
+//             //           ),
+//             //           border: Border.all(
+//             //             color: Colors.black,
+//             //             width: 8,
+//             //           ),
+//             //           borderRadius: BorderRadius.circular(150),
+//             //           boxShadow: [
+//             //             BoxShadow(
+//             //               color: Colors.white.withOpacity(0.5),
+//             //               spreadRadius: 25,
+//             //               blurRadius: 15,
+//             //             ),
+//             //           ]),
+//             //       width: 196,
+//             //       height: 196,
+//             //     ),
+//             //     // Text(
+//             //     //   'Hello World',
+//             //     //   style: TextStyle(fontSize: 24, color: Colors.white),
+//             //     // ),
+//             //   ],
+//             // ),
+//
+//             // ],
+//             // ),
+//             // Row(
+//             //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//             //   mainAxisSize: MainAxisSize.max,
+//             //   children: [
+//             //     Container(
+//             //       color: Colors.purple,
+//             //       width: 201,
+//             //       height: 200,
+//             //     ),
+//             //     Container(
+//             //       color: Colors.lightGreenAccent,
+//             //       width: 201,
+//             //       height: 200,
+//             //     ),
+//             //   ],
+//             // ),
+//           ],
+//         ),
+//       ),
+//     ));
+//   }
+// }
